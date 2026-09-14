5a8b5ca998495e02b452ac34747ca7b2b840689a
Builderforaiworld <horizonai42@gmail.com>
2026-09-14 06:37:37 +0430

    Implement duplicate check and two sum functions
    Added methods to check for duplicates and find two sum in Coding.py.

--- Coding.py
+++ Coding.py
@@ -3,3 +3,26 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
         return nums*2
+# Day 2 
+# Contains Duplicate
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hashset=set()
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
+# two Sum
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j]==target:
+                    return [i,j]
+        return False
+
+
+        
+       
+                 
