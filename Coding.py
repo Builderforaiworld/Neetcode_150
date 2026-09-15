ab549ab62e8e48606a7eb6f9d62228aac77c0774
Builderforaiworld <horizonai42@gmail.com>
2026-09-15 07:43:32 +0430

    Add topKFrequent method to Solution class
    Implement top K frequent elements function.

--- Coding.py
+++ Coding.py
@@ -32,6 +32,25 @@ class Solution:
             res[tuple(count)].append(s)
         return list(res.values())
 
+# day 3 
+top k frequent problem
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count={}
+        for n in nums:
+            count[n]=1+count.get(n,0)
+        freq=[[] for i in range(len(nums)+1)]
+        for n,c in count.items():
+            freq[c].append(n)
+        res=[]
+        for i in range(len(freq)-1,0,-1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res)==k:
+                     return res
+
+                
+
 
         
        
